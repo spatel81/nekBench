2ba2bc7e4a7f9c8b25d6750ca16b979c0a71a974
Saumil Patel <spatel@jlselogin5.ftm.alcf.anl.gov>
2021-10-30 04:20:46 +0000

    add OCCA cmake driver

---
+++ scripts/cbuild_occa.sh
@@ -0,0 +1,71 @@
+# !/bin/bash
+
+CC=icx
+CXX=icpx
+FC=ifx
+
+MPICC=mpiicx
+MPICXX=mpiicpx
+MPIFC=mpiifx
+
+# BUILD_TYPE="Release"
+ENABLE_FORTRAN=OFF
+ENABLE_MPI=OFF
+#ENABLE_DPCPP=OFF
+#ENABLE_OPENCL=OFF
+
+ONEAPI_ROOT=${A21_SDK_ROOT}/compiler/latest/linux
+PREFIX_PATHS="${ONEAPI_ROOT};${ONEAPI_ROOT}/include/sycl;${ONEAPI_ROOT}/compiler"
+
+##BUILD_TYPE="Release"
+###BUILD_TYPE="RelWithDebInfo"
+
+# Default build parameters
+: ${BUILD_DIR:=`pwd`/build}
+: ${INSTALL_DIR:=`pwd`/install}
+: ${BUILD_TYPE:="RelWithDebInfo"}
+#: ${BUILD_TYPE:="Release"}
+: ${CC:="gcc"}
+: ${CXX:="g++"}
+: ${FC:="gfortran"}
+: ${MPICC:="mpicc"}
+: ${MPICXX:="mpicxx"}
+: ${MPIFC:="mpif77"}
+
+# OCCA Configuration
+: ${ENABLE_DPCPP:="ON"}
+: ${ENABLE_OPENCL:="ON"}
+: ${ENABLE_CUDA:="ON"}
+: ${ENABLE_HIP="ON"}
+: ${ENABLE_OPENMP="ON"}
+: ${ENABLE_METAL="ON"}
+: ${ENABLE_MPI="ON"}
+: ${ENABLE_FORTRAN="ON"}
+: ${ENABLE_TESTS="ON"}
+: ${ENABLE_EXAMPLES="ON"}
+
+rm -rf ${BUILD_DIR} ${INSTALL_DIR}
+
+cmake -S . -B ${BUILD_DIR} \
+  -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+  -DCMAKE_PREFIX_PATH=${PREFIX_PATHS} \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  -DCMAKE_Fortran_COMPILER=${FC} \
+  -DMPI_C_COMPILER=${MPICC} \
+  -DMPI_CXX_COMPILER=${MPICXX} \
+  -DMPI_Fortran_COMPILER=${MPIFC} \
+  -DENABLE_OPENMP=${ENABLE_OPENMP} \
+  -DENABLE_OPENCL=${ENABLE_OPENCL} \
+  -DENABLE_DPCPP=${ENABLE_DPCPP} \
+  -DENABLE_CUDA=${ENABLE_CUDA} \
+  -DENABLE_HIP=${ENABLE_HIP} \
+  -DENABLE_METAL=${ENABLE_METAL} \
+  -DENABLE_MPI=${ENABLE_MPI} \
+  -DENABLE_FORTRAN=${ENABLE_FORTRAN} \
+  -DENABLE_TESTS=${ENABLE_TESTS} \
+  -DENABLE_EXAMPLES=${ENABLE_EXAMPLES}
+
+cmake --build ${BUILD_DIR} --parallel 32
+cmake --install ${BUILD_DIR} --prefix ${INSTALL_DIR}
